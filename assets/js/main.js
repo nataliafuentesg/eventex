61b252b823728d6f4da680a56d5f0068fed1d75b
Natalia Fuentes <nataliafuentes@192.168.1.11>
2024-09-04 21:57:01 -0500

    dark mode fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,18 @@ toggleSwitch.addEventListener('change', function() {
   }
 });
 
+// Inicializa el tema al cargar la página
+updateTheme();
 
-
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = bootstrap.Collapse.getOrCreateInstance(menuToggle, {toggle: false})
+navLinks.forEach((l) => {
+    if (menuToggle.classList.contains('show')) {  // only fire on mobile
+        l.addEventListener('click', () => { 
+            bsCollapse.toggle() 
+        })
+    }
+})
 
 
