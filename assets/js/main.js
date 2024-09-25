ddad088050027f16475a139a08b03ef88dfe586f
Natalia Fuentes <nataliafuentes@192.168.1.17>
2024-09-24 23:19:54 -0500

    epps

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,21 +81,55 @@ $(function() {
         navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
     });
 
+    // Inicialización del carrusel de menú
+    var menuOwl = $('.owl-menu');
+    menuOwl.owlCarousel({
+        loop: false,         // No es necesario que sea infinito
+        margin: 10,
+        nav: false,          // Puedes habilitar navegación si lo prefieres
+        dots: false,         // No se necesitan puntos en el menú
+        responsive: {
+            0: {
+                items: 2     // Muestra 2 ítems en móviles
+            },
+            600: {
+                items: 3     // Muestra 3 ítems en pantallas medianas
+            },
+            1000: {
+                items: 4     // Muestra 4 ítems en pantallas grandes
+            }
+        }
+    });
+
+    // Sincronización del menú con el carrusel principal
     var carousel_nav_a = $('.carousel-nav a');
 
-    carousel_nav_a.each(function(slide_index){
+    carousel_nav_a.each(function(slide_index) {
         var $this = $(this);
         $this.attr('data-num', slide_index);
         $this.click(function(e) {
-            owl.trigger('to.owl.carousel',[slide_index,1500]);
+            owl.trigger('to.owl.carousel', [slide_index, 1500]);  // Cambia el slide del carrusel principal
             e.preventDefault();
-        })
-    })
+        });
+    });
 
     owl.on('changed.owl.carousel', function(event) {
         carousel_nav_a.removeClass('active');
-        $(".carousel-nav a[data-num="+event.item.index+"]").addClass('active');
-    })
+        $(".carousel-nav a[data-num=" + event.item.index + "]").addClass('active');  // Cambia la clase activa
+    });
+
+    // Sincronización del carrusel de menú con los clics
+    $('.owl-menu a').click(function(e) {
+        e.preventDefault();
+        var index = $(this).index();
+        
+        // Cambiar el carrusel principal al ítem correspondiente
+        owl.trigger('to.owl.carousel', [index, 300]);
+        
+        // Cambiar la clase activa en el menú
+        $('.owl-menu a').removeClass('active');
+        $(this).addClass('active');
+    });
 
 	
 })
